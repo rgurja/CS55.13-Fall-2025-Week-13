b8c8889095f358c4bf8dbbdc43c1a607fb238a14
rgurja <rgurja@bearcubs.santarosa.edu>
2025-11-24 13:38:57 -0800

    updated posts

--- nextjs-blog/lib/posts.js
+++ nextjs-blog/lib/posts.js
@@ -12,65 +12,61 @@ const dataURL = "https://dev-srjc-fall-2025-cs55-13.pantheonsite.io/wp-json/wp/v
 
 
 export async function getSortedPostsData() {
-   // const filePath = path.join(dataDir, 'posts.json');
-  // const jsonString = fs.readFileSync(filePath, 'utf8');
-  try {
-    //  JSON data from WordPress
-    const jsonString = await got(dataURL);
-    const posts = JSON.parse(jsonString.body);
-    console.log(posts);
-    // Sort posts by date descending
-    const sortedPosts = posts.sort((a, b) => (a.title.rendered < b.title.rendered ? 1 : -1));
-    
+// const filePath = path.join(dataDir, 'posts.json');
+// const jsonString = fs.readFileSync(filePath, 'utf8');
+try {
+// JSON data from WordPress
+const jsonString = await got(dataURL);
+const posts = JSON.parse(jsonString.body);
+console.log(posts);
+// Sort posts by date descending
+const sortedPosts = posts.sort((a, b) => (a.title.rendered < b.title.rendered ? 1 : -1));
 
-    // Return array of objects
-    return sortedPosts.map(post => ({
-      id: post.ID.toString(),
-      title: post.title.rendered,
-      
-    }));
-  } catch (error) {
-    console.log(error);
-    return [];
-  }
+// Return array of objects
+return sortedPosts.map(post => ({
+id: post.id.toString(),
+title: post.title.rendered,
+}));
+} catch (error) {
+console.log(error);
+return [];
+}
 }
 
 export async function getAllPostIds() {
-  try {
-    const jsonString = await got(dataURL);
-    const posts = JSON.parse(jsonString.body);
+try {
+const jsonString = await got(dataURL);
+const posts = JSON.parse(jsonString.body);
 
-    return posts.map(post => ({
-      params: {
-        id: post.ID.toString(),
-        name: post.title.rendered,
-      },
-    }));
-  } catch (error) {
-    console.log( error);
-    return [];
-  }
+return posts.map(post => ({
+params: {
+id: post.id.toString()
+},
+}));
+} catch (error) {
+console.log( error);
+return [];
+}
 }
 
 
 export async function getPostData(id) {
-  const singlePostURL = `https://dev-srjc-fall-2025-cs55-13.pantheonsite.io/wp-json/wp/v2/posts/${id}`;
+const singlePostURL = `https://dev-srjc-fall-2025-cs55-13.pantheonsite.io/wp-json/wp/v2/car/${id}`;
 
-  try {
-    const jsonString = await got(singlePostURL);
-    const post = JSON.parse(jsonString.body);
+try {
+const jsonString = await got(singlePostURL);
+const post = JSON.parse(jsonString.body);
 
-    // Convert post content (HTML) into processed HTML string
-    const processedContent = await remark().use(html).process(post.content.rendered);
-    const contentHtml = processedContent.toString();
+// Convert post content (HTML) into processed HTML string
+const processedContent = await remark().use(html).process(post.content.rendered);
+const contentHtml = processedContent.toString();
 
-    return {
-      id: post.id.toString(),
-      title: post.title.rendered,
-      
-    };
-  } catch (error) {
-    console.log(error);
-    return { id, title: "Post Not Found" };
-  }
+return {
+id: post.id.toString(),
+title: post.title.rendered,
+};
+} catch (error) {
+console.log(error);
+return { id, title: "Post Not Found" };
+}
 }
